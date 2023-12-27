dc611ec7df9e9801dc3f530238698b568033da09
LARRYSALSA <75591878+LARRYICLOUD@users.noreply.github.com>
2023-12-26 21:49:35 -0500

    Agregar archivos de proyecto.

---
+++ FechaNacimiento/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FechaNacimiento
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("YO SOY UN PROGRAMADOR Y EXPERTO EN REDES RECONOCIDO Y EXCELENTE");
+            //Hacer un programa que le pida al usuariola fecha de nacimiento y nosotros le diremos que dia de la semana era
+
+            //Variables necesarias
+            string cadenaFechaNacimiento;
+            DateTime dateTimeFechaNacimiento;
+
+            //Pedimos la fecha de nacimiento
+            Console.Write("Dime la fecha de tu nacimiento (dia mes y año ) y te dire que sia de la semana era:  ");
+            cadenaFechaNacimiento = Console.ReadLine();
+            //Convertimos la cadena en un objeto DateTime
+            dateTimeFechaNacimiento = DateTime.Parse(cadenaFechaNacimiento);
+
+            //Aplicamos formato de fecha personalizado ("dddd") para mostrar el dia de la semana
+            Console.WriteLine($"Naciste un {dateTimeFechaNacimiento.ToString("dddd")}");
+        }
+    }
+}
